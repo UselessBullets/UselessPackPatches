c875a65c051aae5fac2edef1c8983000cae2cbc9
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-11 11:23:53 -0500

    Update ServerGuiMixin.java

--- src/main/java/useless/patches/mixin/ServerGuiMixin.java
+++ src/main/java/useless/patches/mixin/ServerGuiMixin.java
@@ -10,11 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import useless.patches.Patches;
 
 import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 
 @Mixin(value = ServerGui.class,remap = false)
 public class ServerGuiMixin {
 	@Inject(method = "initGui(Lnet/minecraft/server/MinecraftServer;)V", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
 	private static void changeTitle(MinecraftServer minecraftserver, CallbackInfo ci, ServerGui servergui, JFrame jframe){
 		jframe.setTitle("Useless' BTA Modpack Server: " + Patches.ModPackVersion);
+
 	}
 }
