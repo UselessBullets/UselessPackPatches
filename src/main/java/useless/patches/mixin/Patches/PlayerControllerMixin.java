18954da49c8f0104c13c1a755e831d827689ccd3
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-14 18:34:33 -0500

    Removed Break Cooldown

---
+++ src/main/java/useless/patches/mixin/Patches/PlayerControllerMixin.java
@@ -0,0 +1,19 @@
+package useless.patches.mixin.Patches;
+
+import net.minecraft.client.player.controller.PlayerController;
+import net.minecraft.core.util.helper.Side;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = PlayerController.class, remap = false)
+public class PlayerControllerMixin {
+	@Shadow
+	protected int blockHitDelay;
+	@Inject(method = "mine(IIILnet/minecraft/core/util/helper/Side;)V", at = @At("HEAD"))
+	private void removedBreakCooldown(int x, int y, int z, Side side, CallbackInfo ci){
+		blockHitDelay = -1;
+	}
+}
